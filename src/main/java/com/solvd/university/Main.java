355aa3451e748d38c4bf6d95b2264f98a6f31f3d
MichaelRzheutski <michael.rzheutski@gmail.com>
2023-12-26 04:52:23 +0300

    Initial commit

---
+++ src/main/java/com/solvd/university/Main.java
@@ -0,0 +1,9 @@
+package com.solvd.university;
+
+import com.solvd.university.util.menus.AppMainMenu;
+
+public class Main {
+    public static void main(String[] args) {
+        new AppMainMenu().mainMenu();
+    }
+}
